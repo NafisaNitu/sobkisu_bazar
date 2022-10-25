150303442d6b9dbc421160f41115653c2371f0dd
haidercse <haider.cse7644@gmail.com>
2022-10-25 11:55:29 +0600

    add new composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,7 +36,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       
         View::composer('*', function($view){
            $view->with('categories', ModelsCategory::all());
         });
